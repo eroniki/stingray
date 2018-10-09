aa27f3e148755c72acf981aa5e61d37ed2f59f77
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2018-10-09 16:56:27 -0400

    Delete code containing data prep from main.py

--- main.py
+++ main.py
@@ -3,32 +3,6 @@ import os
 import matplotlib.pyplot as plt
 
 
-def prep():
-    fg = frame_grabber()
-
-    cameras = ["data/raw_video/1/",
-               "data/raw_video/2/",
-               "data/raw_video/3/",
-               "data/raw_video/4/"]
-
-    video_list_json = ["data/output/videolist_c1.json",
-                       "data/output/videolist_c2.json",
-                       "data/output/videolist_c3.json",
-                       "data/output/videolist_c4.json"]
-
-    video_list = list()
-
-    for c_id, c in enumerate(cameras):
-        json = fg.find_all_videos(search_path=c,
-                                  location=video_list_json[c_id])
-        videos = fg.load_videolist(video_list_json[c_id])
-        video_list.append(videos)
-        for v_id, video in enumerate(videos["videos"]):
-            video_loc = str(video)
-            print video_loc
-            fg.read_video(video_loc, c_id, test=video, show=False)
-
-
 def main_new():
     fg = frame_grabber()
     tests = ["S5A20.MP4", "S10A50.MP4", "S20A60.MP4"]
